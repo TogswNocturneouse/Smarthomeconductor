fcc6c6dc2590f0111819d4cb8f33150fd098ee83
TheNocturneouse <miklos789@gmail.com>
2026-07-25 03:29:30 +0200

    Add HomeKit adapter foundation

---
+++ Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  SmartHomeConductor
+//
+//  Created by Miklós Ullrich on 2026. 07. 04..
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
